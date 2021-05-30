f7bb2525f440d00e0ef88fb51800548bb8348a80
Abhishek <47608471+deggaronn@users.noreply.github.com>
2021-05-30 15:00:33 +0530

    Add files via upload

---
+++ covid-proj/js/custom.js
@@ -0,0 +1,8 @@
+// event call by id //
+document.querySelector('#btn1').addEventListener('click',()=>{window.location.href=('/oxygenpage.html')});
+document.querySelector('#btn2').addEventListener('click',()=>{window.location.href=('/refilloxygen.html')});
+document.querySelector('#btn3').addEventListener('click',()=>{window.location.href=('/medicine.html')});
+document.querySelector('#btn4').addEventListener('click',()=>{window.location.href=('/food_service.html')});
+document.querySelector('#btn5').addEventListener('click',()=>{window.location.href=('./doctor_cell.html')});
+document.querySelector('#btn6').addEventListener('click',()=>{window.location.href=('ambulance.html')});
+document.querySelector('#btn7').addEventListener('click',()=>{window.location.href=('/bed.html')});
